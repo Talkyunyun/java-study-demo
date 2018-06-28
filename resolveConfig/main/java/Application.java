0643a3a7a264d96fc91e03400c346968daeca6c1
Gene Yang <gene.yang@huolala.cn>
2018-06-28 13:37:19 +0800

    提交代码

---
+++ resolveConfig/main/java/Application.java
@@ -0,0 +1,21 @@
+import com.esotericsoftware.yamlbeans.YamlReader;
+import com.mmlogs.bean.ConfigBean;
+
+import java.io.FileReader;
+
+/**
+ * Class StartApplication
+ * java项目解析配置文件案例
+ * @author Gene.yang
+ * @date 2018/06/28
+ */
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+
+
+        YamlReader reader = new YamlReader(new FileReader(Application.class.getClassLoader().getResource("config.yml").getPath()));
+        ConfigBean contact = reader.read(ConfigBean.class);
+        System.out.println(contact.toString());
+    }
+}
---
+++ resolveConfig/main/java/com/mmlogs/bean/ConfigBean.java
@@ -0,0 +1,17 @@
+package com.mmlogs.bean;
+
+import lombok.Data;
+
+/**
+ * Class Config
+ *
+ * @author Gene.yang
+ * @date 2018/06/28
+ */
+@Data
+public class ConfigBean {
+
+    private String name;
+
+    private int age;
+}
